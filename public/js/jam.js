a50f8034c83edf131bd40c61dc746428d0f83255
twaldorf <loshon@gmail.com>
2017-04-26 15:00:11 -0400

    copy write

--- public/js/jam.js
+++ public/js/jam.js
@@ -2,6 +2,9 @@ var ascene = document.querySelector('a-scene');
 var assets = document.querySelector('a-assets');
 ascene.classList.add("jam-load");
 assets.addEventListener('loaded', unJam);
+var img = document.querySelectorAll('.img');
+img.classList.add("jam-load");
+img.addEventListener('load', unJam);
 
 function unJam() {
   console.log('No more jam!');
