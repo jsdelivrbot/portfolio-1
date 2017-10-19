4c7e96fa854e5fce7657d6445879eafde3522408
Trevor Waldorf <trevor.automated@gmail.com>
2017-10-19 11:46:15 -0400

    add vr links

--- public/js/peanutbutter.js
+++ public/js/peanutbutter.js
@@ -1,22 +1,2 @@
-// document function global whatever
-// fuck jquery man we got peanut butter
-var frame = document.querySelector('.p-frame');
-var count = 0;
-// array picture = element.map('picture') doesn't matter
-frame.addEventListener('click') {
-  frame.setAttribute('src', picture[count]);
-  count++;
-};
-var progress = ul > li
-progress.aEL(){
-  set all to grey
-  add filled class to li[count]
-};
-cool cats dude
-var line = document.querySelector('#hover-line');
-var elements = document.querySelectorAll('.line-hover');
-forEach.elements(this) {
-  this.getVertical height
-  and then
-  line.setAttribute to the one who will rule them all
-};
+var section document.querySelector('#product-container');
+section.addEventListener(mouseenter);
